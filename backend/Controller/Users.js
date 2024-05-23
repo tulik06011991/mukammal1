cd7eba034975ddd8b781b7b28acf3201cd3267fd
realProject <tolqinmirsaliyev@gmail.com>
2024-05-23 12:42:56 +0530

    dgdfgdf

--- backend/Controller/Users.js
+++ backend/Controller/Users.js
@@ -7,7 +7,7 @@ const PostUsers = async (req, res) =>{
     try {
         const user = await pool.query(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *`,
     [username, email, password])
-    res.status(201).json(user.rows[0])
+    return res.status(201).json(user.rows[0])
         
     } catch (error) {
         res.status(500).json(error)
@@ -22,7 +22,7 @@ const getUsers = async (req, res) =>{
         if(!users){
             res.status(404).json(`'ma'lumot topilmadi`)
         }
-        res.status(200).json(users.rows)
+        return res.status(200).json(users.rows)
     } catch (error) {
         res.status(500).json({msg: error})
     }
@@ -35,7 +35,7 @@ const updateUsers = async (req, res) =>{
         const updateUser = await pool.query(`UPDATE products SET username =$1, email =$2, password = $3 WHERE id=$4  RETURNING *`,
             [username, email, password, id]
         );
-        res.status(201).json(updateUser.rows);
+        return res.status(201).json(updateUser.rows);
         if(updateUser.rowCount ===0){
             res.status(404).json(` ma'lumot sozlanmadi`)
         }
@@ -45,10 +45,25 @@ const updateUsers = async (req, res) =>{
         console.log(error)
         
     }
+};
+
+
+const deleteUsers = async (req, res) =>{
+    const {id}= req.params
+    try {
+        const deleteUser = await pool.query(` DELETE FROM   users WHERE ID=$1 RETURNING *`,[id]);
+        return res.status(200).send('ma"lumot o"chirildi')
+        if (deleteUser.rowCount === 0) {
+            return res.status(404).send('Product not found');
+        }
+    } catch (error) {
+        
+    }
 }
 
 
 module.exports = {
     PostUsers,
-    getUsers
+    getUsers,
+    updateUsers
 };
