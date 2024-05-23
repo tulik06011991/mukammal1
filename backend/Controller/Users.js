a8ea441027de4cfc058243819cd99c41a17340f2
realProject <tolqinmirsaliyev@gmail.com>
2024-05-23 12:34:54 +0530

    vxgfdxg

--- backend/Controller/Users.js
+++ backend/Controller/Users.js
@@ -19,9 +19,31 @@ const PostUsers = async (req, res) =>{
 const getUsers = async (req, res) =>{
     try {
         const users = await pool.query(`SELECT * FROM users`);
+        if(!users){
+            res.status(404).json(`'ma'lumot topilmadi`)
+        }
         res.status(200).json(users.rows)
     } catch (error) {
         res.status(500).json({msg: error})
+    }
+}
+
+
+const updateUsers = async (req, res) =>{
+    const {id} = req.params
+    try {
+        const updateUser = await pool.query(`UPDATE products SET username =$1, email =$2, password = $3 WHERE id=$4  RETURNING *`,
+            [username, email, password, id]
+        );
+        res.status(201).json(updateUser.rows);
+        if(updateUser.rows.length ===0){
+            res.status(404).json(` ma'lumot sozlanmadi`)
+        }
+        
+    } catch (error) {
+        res.status(200).json({msg: error})
+        console.log(error)
+        
     }
 }
 
