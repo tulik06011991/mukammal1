e51d3a9ad95677afd170e7cd5f573f8ac277196c
tulqin <tolqinmirsaliyev@gmail.com>
2024-05-28 23:57:23 +0500

    wewrwer

--- backend/middleware.js/middleware.js
+++ backend/middleware.js/middleware.js
@@ -1,1 +1,14 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+
+const middleware = (req, res, next) =>{
+    const auth = req.headers('Authorization');
+    if(!auth) throw new Error('no token');
+    try {
+        const token = jwt.sign(auth, process.env.JWT_SECRET);
+        req.user = token;
+        next()
+    } catch (error) {
+        throw new Error (`xato token`)
+        
+    }
+}
