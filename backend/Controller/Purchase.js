68e4bd6a182e0d71ce2bff3ec6669d9ff7428eca
tulqin <tolqinmirsaliyev@gmail.com>
2024-05-28 22:38:17 +0500

    sdsds

--- backend/Controller/Purchase.js
+++ backend/Controller/Purchase.js
@@ -1,2 +1,23 @@
-const pool = require('../db')
+const pool = require('../db');
+
+
+const purchases = async (req, res) =>{
+    try {
+        const result = await pool.query(`
+        SELECT user_id, product_name, SUM(quantity) as total_quantity
+        FROM sales
+        GROUP BY user_id, product_name
+      `);
+      res.json(result.rows);
+        
+    } catch (error) {
+       console.log(error);
+       res.status(500).json({msg: error}) 
+    }
+
+};
+
+
+
+module.export = purchases
 
