a0ade71d439e253aa79069a3d6ffc5ab4c3b648d
tulqin <tolqinmirsaliyev@gmail.com>
2024-05-29 07:29:10 +0500

    ddfdfdf

--- backend/middleware.js/middleware.js
+++ backend/middleware.js/middleware.js
@@ -1,28 +1,25 @@
 const jwt = require("jsonwebtoken");
 
-const middleware = (req, res, next) =>{
-    const auth = req.headers('Authorization');
-    if(!auth) throw new Error('no token');
+const verifyToken = (req, res, next) => {
+    const token = req.headers['access-token'];
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
     try {
-        const token = jwt.sign(auth, process.env.JWT_SECRET);
-        req.user = token;
-        next()
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
     } catch (error) {
-        throw new Error (`xato token`)
-        
+        return res.status(400).json({ message: 'Invalid token' });
     }
 };
 
-const checkAdmin = (req, res, next) =>{
-    try {
-        if(req.user && req.user.isAdmin){
-            next()
-        }
-    } catch (error) {
-        throw new Error('siz admin emassiz')
-        
+const checkAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        return res.status(403).json({ message: 'Access forbidden: You are not an admin' });
     }
-}
+};
 
-
-module.exports = {checkAdmin, middleware}
+module.exports = { checkAdmin, verifyToken };
