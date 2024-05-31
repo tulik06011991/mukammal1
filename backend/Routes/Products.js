48fc26ba9aa54f0f12b35175bd44c09f53688e14
tulqin <tolqinmirsaliyev@gmail.com>
2024-05-31 15:36:27 +0500

    sfsdsds

--- backend/Routes/Products.js
+++ backend/Routes/Products.js
@@ -1,33 +1,35 @@
 const expres = require('express')
 const {AllProducts} = require('../Controller/Products')
 const {upload} = require('../middleware.js/middleware')
-
+const pool = require("../db")
 const router = expres.Router();
 
 router.get('/products', AllProducts);
 
-
-router.post('/postProducts', upload.single('image'), async (req, res) =>{
+router.post('/postProducts', upload.single('image'), async (req, res) => {
     const { name, description, price } = req.body;
-    const filePath = req.file.path;
-    const fileName = req.file.filename;
-    
-    if (!name || !description || typeof price !== 'number') {
+    const filePath = req.file ? req.file.path : null;
+    const fileName = req.file ? req.file.filename : null;
+
+    // Price ni raqamga o'girish
+    const parsedPrice = parseFloat(price);
+
+    if (!name || !description || isNaN(parsedPrice)) {
         return res.status(400).send('Invalid input');
     }
 
     try {
         const product = await pool.query(
-            'INSERT INTO products(name, description, price,  image_name, image_path) VALUES ($1, $2, $3) RETURNING *',
-            [name, description, price, fileName, filePath]
+            'INSERT INTO products (name, description, price, image_name, image_path) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+            [name, description, parsedPrice, fileName, filePath]
         );
         res.status(201).json(product.rows[0]);
     } catch (error) {
         console.error('Error adding product:', error);
         res.status(500).send('Error adding product');
     }
+});
 
-})
 
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import {Route, Routes} from 'react-router-dom'
 import './index.css'
 import Navbar from './components/Navbar'
+import Menu from './components/Menu'
+import Footer from './components/Footer'
+import Contact from './components/Contact'
 
 function App() {
  
