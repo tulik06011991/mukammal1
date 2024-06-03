8def411f1b34ec49c6d59eb41c96d4986faafaa9
tulqin <tolqinmirsaliyev@gmail.com>
2024-06-03 21:55:02 +0500

    dsfdsf

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -29,7 +29,7 @@ const Menu = () => {
             <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
                 {Array.isArray(data) && data.length > 0 ? (
                     data.map((d, i) => (
-                        <div key={i} className="max-w-sm rounded overflow-hidden shadow-lg m-3 flex flex-col justify-between" style={{ height: "450px" }} onClick={handleClick}>
+                        <div key={i} className="max-w-sm rounded overflow-hidden shadow-lg m-3 flex flex-col justify-between hover:cursor-pointer" style={{ height: "450px" }} onClick={handleClick}>
                             <img className="w-full mx-auto mt-2" style={{ height: "180px", width: '150px' }} src={d.image} alt={d.title} />
                             <div className="px-6 py-4">
                                 <div className="font-bold text-xl mb-2">{d.title}</div>
