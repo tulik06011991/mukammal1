f14350990e11a1b3c855c2a8fd4e834069f78c7d
tulqin <tolqinmirsaliyev@gmail.com>
2024-06-03 21:53:38 +0500

    edfewrewrwe

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,13 +1,18 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import productContext from './context/ProductContext';
 
 const Menu = () => {
+    const [isButtonChecked, setIsButtonChecked] = useState(false);
     const [data, setData] = useState([]);
-    const { setProducts, setIsSidebarOpen } = useContext(productContext);
+    const { setIsSidebarOpen } = useContext(productContext);
+
+    const handleClick = () => {
+        setIsSidebarOpen(false);
+    };
 
     useEffect(() => {
-        const getProducts = async () => {
+        const fetchData = async () => {
             try {
                 const response = await axios.get('https://fakestoreapi.com/products');
                 setData(response.data);
@@ -15,20 +20,16 @@ const Menu = () => {
                 console.error("Error fetching data: ", error);
             }
         };
-        getProducts();
-    }, []);
 
-    const handleCardClick = (product) => {
-        setProducts((prevProducts) => [...prevProducts, product]);
-        setIsSidebarOpen(false); // Sidebarni yopish
-    };
+        fetchData();
+    }, []);
 
     return (
         <div>
             <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
                 {Array.isArray(data) && data.length > 0 ? (
                     data.map((d, i) => (
-                        <div key={i} className="max-w-sm rounded overflow-hidden shadow-lg m-3 flex flex-col justify-between" style={{ height: "450px" }}>
+                        <div key={i} className="max-w-sm rounded overflow-hidden shadow-lg m-3 flex flex-col justify-between" style={{ height: "450px" }} onClick={handleClick}>
                             <img className="w-full mx-auto mt-2" style={{ height: "180px", width: '150px' }} src={d.image} alt={d.title} />
                             <div className="px-6 py-4">
                                 <div className="font-bold text-xl mb-2">{d.title}</div>
@@ -40,7 +41,6 @@ const Menu = () => {
                                 <button
                                     type="button"
                                     className='rounded-full mx-auto px-3 py-1 cursor-pointer text-sm font-semibold bg-gray-500 hover:bg-gray-700'
-                                    onClick={() => handleCardClick(d)}
                                 >
                                     Add to Cart
                                 </button>
