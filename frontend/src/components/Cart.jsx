a40124ccda65d4fa60d1b8690a1ba426e9fab123
tulqin <tolqinmirsaliyev@gmail.com>
2024-06-05 16:15:45 +0500

    frontend tayyor

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -62,7 +62,7 @@ const Cart = () => {
         </div>
         <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-400">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
     </div>
-    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
 </form>
 
 	<p className="mt-5">These input field components is part of a larger, open-source library of Tailwind CSS components. Learn
