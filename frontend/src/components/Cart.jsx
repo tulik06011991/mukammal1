c84c74a949010080849341df555976225dd13ac0
tulqin <tolqinmirsaliyev@gmail.com>
2024-06-05 16:53:34 +0500

    gdsdgdhs

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -40,7 +40,7 @@ const Cart = () => {
          
         <div>
             <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Phone number</label>
-            <input type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" pattern="[0-9]{0-9}-[0-8]{3}-[2]{2}" required/>
+            <input type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" pattern="/^\+998[0-9]{9}$/" required/>
         </div>
         
     </div>
